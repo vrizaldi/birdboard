452dbdd72f85fcaa78a80790ab33efb60df7bb04
vrizaldi <vrizaldi@gmail.com>
2020-05-22 21:39:11 +0700

    can index and store projects

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    public function index() {
+        $projects = Project::all();
+        return view('projects.index', ['projects' => $projects]);
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Project::create($attributes);
+        return redirect('/projects');
+    }
+}
